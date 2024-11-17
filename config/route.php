ac4ab765c2473fb5750af6e8435dede0de11c3c0
zjkal <javachina@foxmail.com>
2024-11-17 13:34:27 +0800

    将v1签名url重定向到v2版本

--- config/route.php
+++ config/route.php
@@ -17,5 +17,4 @@ use Webman\Route;
 // 禁用路由
 Route::disableDefaultRoute();
 Route::any('/', [app\controller\IndexController::class, 'index']);
-Route::any('/{username:[a-zA-Z0-9._-]+}.png', [app\controller\ApiController::class, 'index']);
-Route::any('/{username:[a-zA-Z0-9._-]+}.svg', [app\controller\ApiController::class, 'v2']);
+Route::any('/{username:[a-zA-Z0-9._-]+}.(png|svg)', [app\controller\ApiController::class, 'v2']);
